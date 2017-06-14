3c505755dc9d50dbe661fcdc93eed12a07464009
Rohan Bhatia <rohanbhatia96@outlook.com>
2017-06-14 15:35:26 +0530

    remove mail settings credentials

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,8 +32,8 @@ Blog::Application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'localhost:3000',
-    user_name:            'rohantester10@gmail.com',
-    password:             'rohantester123',
+    user_name:            'Email',
+    password:             'Password',
     authentication:       'plain',
     enable_starttls_auto: true  }
 end
